56b19934ce591630a0a11047e4c50f2a28249f5d
Ramon Chavez <rachavez1974@gmail.com>
2023-06-14 13:27:29 -0400

    Add faker gem to development and seed user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+  user = {
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone,
+    address: Faker::Address.full_address,
+    title: Faker::Job.title
+  }
+  User.create(user)
+end
