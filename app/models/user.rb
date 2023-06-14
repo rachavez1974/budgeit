ab627ab41358a80348e2448ea9dcebfb6212215d
Ramon Chavez <rachavez1974@gmail.com>
2023-06-14 13:55:18 -0400

    Add csv gem to development, and create class method to make csv file for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  def self.create_user_csv
+    users = User.all
+
+    CSV.open('./lib/files/user.csv', 'wb') do |csv|
+      csv << ["Name", "Email", "Phone", "Address", "Title"]
+      users.each do |user|
+        csv << [user.name, user.email, user.phone, user.address, user.title]
+      end
+    end
+  end
 end
